f2d2a6ed467cc949023ec9bae13b996c4a7d0c21
CORTINOVIS.21123 <cortinovis.matteo.studente88@itispleocapa.it>
2025-03-10 13:55:11 +0100

    aggiunta file

--- src/Gestore.java
+++ src/Gestore.java
@@ -1,3 +1,27 @@
-public class Gestore {
+import java.util.ArrayList;
+import java.io.*;
+import java.util.ArrayList;
 
+public class Gestore {
+    private ArrayList<Record> lista;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private File fileNuovo;
+
+    public Gestore() throws IOException {
+        this.lista=new ArrayList<Record>();
+        this.writer=new PrintWriter(new FileWriter(this.fileNuovo));
+        this.reader=new BufferedReader(new FileReader(this.fileNuovo));
+    }
+    public void add(Record record) {
+        this.lista.add(record);
+    }
+
+    public void addCampo(){
+        File nuovo
+    }
+    @Override
+    public String toString() {
+        return "Risultati{" + "lista=" + lista + '}';
+    }
 }
