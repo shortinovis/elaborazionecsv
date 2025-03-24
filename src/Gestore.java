399ecee5a4e19be98faadba173e573e56441fc17
CORTINOVIS.21123 <cortinovis.matteo.studente88@itispleocapa.it>
2025-03-24 13:18:37 +0100

    aggiunta primi 3 metodi

--- src/Gestore.java
+++ src/Gestore.java
@@ -1,27 +1,68 @@
 import java.util.ArrayList;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Gestore {
-    private ArrayList<Record> lista;
-    private PrintWriter writer;
-    private BufferedReader reader;
-    private File fileNuovo;
+    private File fileMio;
 
-    public Gestore() throws IOException {
-        this.lista=new ArrayList<Record>();
-        this.writer=new PrintWriter(new FileWriter(this.fileNuovo));
-        this.reader=new BufferedReader(new FileReader(this.fileNuovo));
-    }
-    public void add(Record record) {
-        this.lista.add(record);
+    public Gestore(File fileMio) {
+        this.fileMio = fileMio;
     }
 
-    public void addCampo(){
-        File nuovo
+    public void addCampo() throws RuntimeException, IOException {
+        File file = this.fileMio;
+        File fileappoggio = new File("src/Cortinovis");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.fileMio));PrintWriter writer=new PrintWriter(new FileWriter(fileappoggio)))
+        {
+            Boolean primaLinea=true;
+            String next;
+            while ((next= reader.readLine())!=null){
+                if(primaLinea) {
+                    writer.println(next + "; mioparametro; cancellazionelogica");
+                    primaLinea = false;
+                }else {
+                    writer.println(next +"; " + (new Random().nextInt(11)+10)+ ";" + true);
+                }
+                writer.flush();
+                writer.close();
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        if (!file.delete()) {
+            throw new RuntimeException("Errore nella cancellazione del file originale");
+        }
+        if (!fileappoggio.renameTo(fileMio)) {
+            throw new RuntimeException("Errore nel rinominare il nuovo file come quello originale");
+        }
     }
-    @Override
-    public String toString() {
-        return "Risultati{" + "lista=" + lista + '}';
+
+    public int contaCampi() throws RuntimeException{
+        try (BufferedReader reader=new BufferedReader(new FileReader(this.fileMio))){
+            String [] recordControllo=reader.readLine().split(";");
+            return recordControllo.length;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public int maxRecord() throws RuntimeException{
+        try (BufferedReader reader=new BufferedReader(new FileReader(this.fileMio))){
+            reader.readLine(); //Elimino l'intestazione dal conteggio
+
+            int max=0;
+            String next;
+            while ((next = reader.readLine()) != null){
+                if (next.length()>max)
+                    max=next.length();
+            }
+            return max;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
+
